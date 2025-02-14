552d82ef9092297da7c8ddda1d5a188186c51e38
Fabrice <f.nkurunziz@alustudent.com>
2025-02-14 18:36:04 +0200

    define the user model

---
+++ models/user.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema(
+	{
+		fname: {
+			type: String,
+			required: true,
+		},
+		lname: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
